85a8351e3b9b3809b7bf6cebda301a3fbdae2335
Matheus-Barrales <106543248+Matheus-Barrales@users.noreply.github.com>
2022-10-03 13:30:39 -0300

    Add files via upload

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+function Home(){
+    return(
+        <div class = "row">
+            <div class="column">
+                <div class="card">
+                    <h1>Página Inicial</h1>
+                    <h2>Página inicial do Banco PW</h2>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Home;
---
+++ src/Pages/SobreNos/index.js
@@ -0,0 +1,13 @@
+function SobreNos(){
+    return(
+        <div>
+            <h1>• Um poco sobre nós:</h1>
+            <p> Nossa escola de programação PCProg tem a intenção de fazer o mercado
+                crescer cada vez mais e dar mais chances e oportunidades àqueles
+                que se interessam pela área.
+            </p>
+            <img src="Imagens.imagem.webp" alt="PCProg"></img>
+        </div>
+    )
+}
+export default SobreNos;
